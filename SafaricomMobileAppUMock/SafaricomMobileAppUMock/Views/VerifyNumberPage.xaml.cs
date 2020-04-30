b9cb5e12dd274a1c9173ccc7045a8742f2385a9c
John Kimoi <jhnkimoigechure@gmail.com>
2020-04-30 09:33:31 +0300

    transferred everything to resources in xaml, removed extra font classes

--- SafaricomMobileAppUMock/SafaricomMobileAppUMock/Views/VerifyNumberPage.xaml.cs
+++ SafaricomMobileAppUMock/SafaricomMobileAppUMock/Views/VerifyNumberPage.xaml.cs
@@ -24,10 +24,10 @@ namespace SafaricomMobileAppUMock.Views
             txtPhoneNumber.VerticalTextAlignment = TextAlignment.Center;
             txtPhoneNumber.FontAttributes = FontAttributes.Bold;
             txtPhoneNumber.TextColor = Application.Current.Resources["Primary"]!=null? (Color)Application.Current.Resources["Primary"] : Color.DarkCyan;
-            txtPhoneNumber.FontFamily = "BwGlennSansDEMO-Bold";
+            txtPhoneNumber.FontFamily = "CenturyGothicBold";
             btnGeneratePin.FontFamily = "BwGlennSansDEMO-Bold";
            // btnGeneratePin.FontFamily = "CenturyGothicBold";
-            btnGeneratePin.FontSize = 18;
+            btnGeneratePin.FontSize = 20;
             
             lblEnterEmailMessage.HorizontalTextAlignment = TextAlignment.Center;
             txtTapMessage.HorizontalTextAlignment = TextAlignment.Center;
