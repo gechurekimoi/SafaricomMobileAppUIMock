3aa10949993a7e297ef1918aacf42529a6044f98
John Gechure Kimoi <gechurejohn@gmail.com>
2020-05-03 09:43:13 +0300

    worked on sliding menu colors

--- SafaricomMobileAppUMock/SafaricomMobileAppUMock.iOS/AppDelegate.cs
+++ SafaricomMobileAppUMock/SafaricomMobileAppUMock.iOS/AppDelegate.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using FFImageLoading.Forms.Platform;
 using Foundation;
 using UIKit;
 
@@ -24,7 +24,7 @@ namespace SafaricomMobileAppUMock.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
-
+            CachedImageRenderer.Init();
             return base.FinishedLaunching(app, options);
         }
     }
